cfccfd2eaa61d8ba994ebb645146a00b69d2fbf5
robux4 <slhomme@matroska.org>
2010-09-26 08:19:05 +0000

    add support for "make install" on UNIX systems use GNUmakefile instead of Makefile as some BSD systems don't use GNU make better build.sh to build and install coremake on a UNIX system
    git-svn-id: https://dell990mt-pc/svn/coremake/trunk@448 71262bcb-1727-0410-9a64-faf002f7cc9f

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-sudo gcc -O2 coremake.c -o /usr/local/bin/coremake
-sudo gcc -O2 corerc.c -o /usr/local/bin/corerc
+make -f build.mak
+sudo mv -f coremake /usr/local/bin/coremake
+sudo mv -f corerc /usr/local/bin/corerc
+
+#sudo gcc -O2 coremake.c -o /usr/local/bin/coremake
+#sudo gcc -O2 corerc.c -o /usr/local/bin/corerc
